abb1b7403918ebbbed26cf771cf32202c765b662
Pekaby <11gavegames11@gmail.com>
2020-10-18 21:44:39 +0300

    Create debug.php

---
+++ debug.php
@@ -0,0 +1,21 @@
+<?php
+function debug($var = '')
+{
+    global $_SESSION;
+    global $requests;
+
+    if ($var == '') {
+        echo "<pre>";
+        print_r($requests);
+        echo "Path __DIR__: " . __DIR__ . "\n";
+        echo "SESSION: ";
+        var_dump($_SESSION);
+        echo "COOKIE: ";
+        print_r($_COOKIE);
+        echo "</pre>";
+    }else{
+        echo "<pre>";
+        print_r($var);
+        echo "</pre>";
+    }
+}
